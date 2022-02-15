338d89e2e33d20ea06f54ca7bd6009b29679582b
2200144Yuusuke <2200144@ecccomp.com>
2022-02-15 20:48:22 +0900

    敵作るはじめ

--- sources/BaseEnemy.h
+++ sources/BaseEnemy.h
@@ -1,1 +1,35 @@
 #pragma once
+#include"graphics_pipeline.h"
+#include"skinned_mesh.h"
+#include<memory>
+//****************************************************************
+// 
+// “G‚ÌŠî’êƒNƒ‰ƒX 
+// 
+//****************************************************************
+
+class BaseEnemy
+{
+    //****************************************************************
+    // 
+    // ŠÖ”
+    // 
+    //****************************************************************
+public:
+    BaseEnemy();
+    virtual ~BaseEnemy() = default;
+
+    virtual void fInitialize() = 0;
+    virtual void fUpdate(GraphicsPipeline GPipeline_, float elapsedTime_) = 0;
+    void fRender(GraphicsPipeline gPipeline_);
+
+    //****************************************************************
+    // 
+    // •Ï” 
+    // 
+    //****************************************************************
+private:
+    // ƒ‚ƒfƒ‹
+    std::unique_ptr<SkinnedMesh> mpSkinnedMesh{ nullptr };
+
+};
