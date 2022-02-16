adece0ff6c6716fdfcd25fddb84133f9b91e0fc9
kanbeshouma <yyskanbe@gmail.com>
2022-02-16 13:22:50 +0900

    Update BasePlayer.h
    Playerの基底クラス作成

--- sources/BasePlayer.h
+++ sources/BasePlayer.h
@@ -1,1 +1,21 @@
 #pragma once
+#include<memory>
+#include<DirectXMath.h>
+#include"skinned_mesh.h"
+
+class BasePlayer
+{
+public:
+    BasePlayer() {}
+    virtual ~BasePlayer() {}
+protected:
+    virtual  void Initialize() = 0;
+    virtual  void Update(float elapsed_time) = 0;
+    virtual void Render(float elapsed_time) = 0;
+protected:
+    DirectX::XMFLOAT3 position{};
+    DirectX::XMFLOAT4 orientation{ 0,0,0,1.0f };
+    DirectX::XMFLOAT3 scale{ 1.0f,1.0f,1.0f };
+protected:
+    std::shared_ptr<SkinnedMesh> model{ nullptr };
+};
