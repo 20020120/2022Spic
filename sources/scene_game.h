3582367ac9edcc0d11758d76bb0fd5955b7b1f82
umiyuriktn <2200114@ecccomp.com>
2022-02-16 14:10:02 +0900

    シーンゲーム整備
    シーンゲーム整備

--- sources/scene_game.h
+++ sources/scene_game.h
@@ -35,22 +35,12 @@ public:
     void register_shadowmap(GraphicsPipeline& graphics, float elapsed_time) override;
 private:
     //--------< 変数 >--------//
-    float gravity = 0;
     // camera
     std::unique_ptr<Camera> camera;
     // shadowmap
     std::unique_ptr<ShadowMap> shadow_map;
     // post effect
     std::unique_ptr<PostEffect> post_effect;
-    // fog
-    std::unique_ptr<FullScreenQuad> fog_post_effect;
-    Microsoft::WRL::ComPtr<ID3D11PixelShader> fog_ps;
-    struct MistConstants
-    {
-        DirectX::XMFLOAT4 fog_color{ 1,1,1,1 };
-        DirectX::XMFLOAT4 fog_range{ 0.1f, 30.0f, 0,0 }; // x:near y:far
-    };
-    std::unique_ptr<Constants<MistConstants>> mist_constants;
     // bloom
     std::unique_ptr<Bloom> bloom_effect;
     struct BloomConstants
@@ -61,41 +51,14 @@ private:
         float bloom_options[2];
     };
     std::unique_ptr<Constants<BloomConstants>> bloom_constants;
-    // PointLights
-    std::unique_ptr<PointLights> point_lights;
     // スキンメッシュの実体
-    std::shared_ptr<SkinnedMesh> skinned_meshes[5];
-
+    std::shared_ptr<SkinnedMesh> sky_dome;
+    std::shared_ptr<SkinnedMesh> test;
     std::unique_ptr<Effect> test_effect;
-    std::unique_ptr<Effect> test_effect2;
-
-    struct Player
-    {
-        DirectX::XMFLOAT3 position{};
-        DirectX::XMFLOAT3 scale{};
-        DirectX::XMFLOAT3 angle{};
-        int anim_index{ 0 };
-    } player;
-    bool display_player_imgui = false;
-
-    struct Stage
-    {
-        DirectX::XMFLOAT3 position{};
-        DirectX::XMFLOAT3 scale{};
-        DirectX::XMFLOAT3 angle{};
-        DirectX::XMFLOAT4X4 coordinate{};
-    } stage;
-
-    bool accumulate_husk_particles{ true };
-    bool integrate_particles{ false };
-    std::unique_ptr<HuskParticles> particles;
-
-
     //****************************************************************
-    // 
-    // スピック開始後追加した変数 
-    // 
+    //
+    // スピック開始後追加した変数
+    //
     //****************************************************************
-
     EnemyManager mEnemyManager{};
 };
