cbbb3eff17b895ae585b6a73b175f8cfa787f8ff
umiyuriktn <2200114@ecccomp.com>
2022-03-30 02:45:53 +0900

    player モデル適応
    player モデル適応

--- sources/Player.cpp
+++ sources/Player.cpp
@@ -7,7 +7,7 @@
 Player::Player(GraphicsPipeline& graphics)
     :BasePlayer()
 {
-    model = resource_manager->load_model_resource(graphics.get_device().Get(), ".\\resources\\Models\\Player\\player_fourteenth.fbx");
+    model = resource_manager->load_model_resource(graphics.get_device().Get(), ".\\resources\\Models\\Player\\player_fifteenth.fbx");
     model->play_animation(AnimationClips::Idle, true);
     scale = { 0.06f,0.06f,0.06f };
     GetPlayerDirections();
