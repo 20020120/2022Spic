ea483c6b459756063dcea5ebe97b997c04923b44
umiyuriktn <2200114@ecccomp.com>
2022-04-21 22:44:19 +0900

    tunnel_ps 追加
    tunnel_ps 追加

---
+++ sources/tunnel.cpp
@@ -0,0 +1,16 @@
+//#include "shader.h"
+//#include "texture.h"
+//
+//#include "tunnel.h"
+//
+//Tunnel::Tunnel(ID3D11Device* device)
+//{
+//    // ps
+//    const char* cso_name_ps{ "shaders/tunnel_ps.cso" };
+//    create_ps_from_cso(device, cso_name_ps, pixel_shader.GetAddressOf());
+//    // srv
+//    D3D11_TEXTURE2D_DESC texture2d_desc{};
+//    load_texture_from_file(device, L".\\resources\\TexMaps\\tunnel\\tunnel.png", texture_map.GetAddressOf(), &texture2d_desc);
+//
+//
+//}
--- sources/tunnel.h
+++ sources/tunnel.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <d3d11.h>
+#include <wrl.h>
+
+class Tunnel
+{
+public:
+    //--------<constructor/destructor>--------//
+    Tunnel(ID3D11Device* device);
+    ~Tunnel() = default;
+    //--------< ŠÖ” >--------//
+
+
+private:
+    //--------< •Ï” >--------//
+    Microsoft::WRL::ComPtr<ID3D11PixelShader> pixel_shader;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> texture_map;
+};
