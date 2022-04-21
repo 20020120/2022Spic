22de25ea4847ab416a7b0e26297d0e2edb5b5308
2200144Yuusuke <2200144@ecccomp.com>
2022-04-21 16:29:31 +0900

    ファイルそうさ

---
+++ sources/SwordEnemyAce.cpp
@@ -0,0 +1,16 @@
+#include"SwordEnemyAce.h"
+
+void SwordEnemyAce::fRegisterFunctions()
+{
+    {
+        InitFunc init = [=]()->void
+        {
+            fStartInit();
+        };
+        UpdateFunc upd = [=](float elapsedTime_, GraphicsPipeline& Graphics_)->void
+        {
+            fStartUpdate(Graphics_, elapsedTime_);
+        };
+        mFunctionMap.insert(DivideState::Start, std::make_tuple(init,upd ));
+    }
+}
---
+++ sources/SwordEnemyAce.h
@@ -0,0 +1,59 @@
+#pragma once
+#include"BaseEnemy.h"
+#include "EnemiesEditor.h"
+//****************************************************************
+// 
+// 剣の敵の中ボス 
+// 
+//****************************************************************
+class SwordEnemyAce final :public BaseEnemy
+{
+public:
+    struct DivideState
+    {
+        inline static const std::string CounterBegin     = "CounterBegin";      // カウンターはじめ
+        inline static const std::string CounterMiddle    = "CounterMiddle";     // カウンター受付中
+        inline static const std::string CounterEnd       = "CounterEnd";        // カウンター受付終了
+        inline static const std::string CounterActivate  = "CounterActivate";   // カウンター攻撃始動
+        inline static const std::string CounterAttack    = "CounterAttack";     // カウンター攻撃
+        inline static const std::string CounterAttackEnd = "CounterAttackEnd";  // カウンター攻撃終了
+    };
+    //****************************************************************
+    // 
+    // 関数
+    // 
+    //****************************************************************
+    SwordEnemyAce(GraphicsPipeline& graphics_, int UniqueId_,
+        DirectX::XMFLOAT3 EmitterPoint_/*スポーン位置*/,
+        ParamGetFunction Func_);
+    ~SwordEnemyAce() override = default;
+
+    void fUpdate(GraphicsPipeline& Graphics_, float elapsedTime_) override;
+    void fRegisterFunctions() override; // ステートを登録
+
+    void fSetAttackCapsuleCollider();
+
+    void fSetVernierEffectPos();
+
+    void fStopEffect() override;
+    //****************************************************************
+    // 
+    // 変数
+    // 
+    //****************************************************************
+
+    //****************************************************************
+    // 
+    // ステートマシン
+    // 
+    //****************************************************************
+    //--------------------<登場>--------------------//
+    void fStartInit();
+    void fStartUpdate(GraphicsPipeline& Graphics_, float elapsedTime_);
+    //--------------------<待機>--------------------//
+
+
+    //--------------------<移動>--------------------//
+
+};
+
