6f70d1389bb2dc8c13dce919eb05ddc74a195a44
umiyuriktn <2200114@ecccomp.com>
2022-04-25 19:42:06 +0900

    player chain 関数実装
    player chain 関数実装

--- sources/PlayerChain.cpp
+++ sources/PlayerChain.cpp
@@ -3,3 +3,43 @@
 void Player::ChainLockOn()
 {
 }
+
+void Player::chain_search_update(float elapsed_time)
+{
+
+}
+
+void Player::transition_chain_search()
+{
+
+}
+
+void Player::chain_lockon_update(float elapsed_time)
+{
+
+}
+
+void Player::transition_chain_lockon()
+{
+
+}
+
+void Player::chain_move_update(float elapsed_time)
+{
+
+}
+
+void Player::transition_chain_move()
+{
+
+}
+
+void Player::chain_attack_update(float elapsed_time)
+{
+
+}
+
+void Player::transition_chain_attack()
+{
+
+}
