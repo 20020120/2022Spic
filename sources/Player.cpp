1765b9297ae55d5181f4b2e5ab84841d509fb4b8
kanbeshouma <yyskanbe@gmail.com>
2022-05-01 18:57:32 +0900

    プレイヤー
    モデル変更

--- sources/Player.cpp
+++ sources/Player.cpp
@@ -7,7 +7,7 @@
 Player::Player(GraphicsPipeline& graphics)
     :BasePlayer()
 {
-    model = resource_manager->load_model_resource(graphics.get_device().Get(), ".\\resources\\Models\\Player\\player_seventeenth.fbx",true,60.0f);
+    model = resource_manager->load_model_resource(graphics.get_device().Get(), ".\\resources\\Models\\Player\\player_eighteenth.fbx",false,60.0f);
     TransitionIdle();
     scale = { 0.06f,0.06f,0.06f };
     GetPlayerDirections();
