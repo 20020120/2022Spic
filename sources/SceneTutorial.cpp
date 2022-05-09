fbae73fa7540371ca73bf52cf10d20da68cb0e26
kanbeshouma <yyskanbe@gmail.com>
2022-05-09 11:36:10 +0900

    チュートリアル
    チュートリアルの必要なテキスト

---
+++ sources/SceneTutorial.cpp
@@ -0,0 +1,494 @@
+#include "SceneTutorial.h"
+
+#include <memory>
+#include "scene_title.h"
+#include "scene_loading.h"
+#include "scene_manager.h"
+
+#include "framework.h"
+#include "graphics_pipeline.h"
+
+#include "collision.h"
+#include "shader.h"
+#include "texture.h"
+
+#include "user.h"
+
+
+void TutorialScene::initialize(GraphicsPipeline& graphics)
+{
+	// shadow_map
+	shadow_map = std::make_unique<ShadowMap>(graphics);
+	// post effect
+	post_effect = std::make_unique<PostEffect>(graphics.get_device().Get());
+	// bloom
+	bloom_effect = std::make_unique<Bloom>(graphics.get_device().Get(), SCREEN_WIDTH, SCREEN_HEIGHT);
+	bloom_constants = std::make_unique<Constants<BloomConstants>>(graphics.get_device().Get());
+	// モデルのロード
+	sky_dome = std::make_unique<SkyDome>(graphics);
+	// effect
+	test_effect = std::make_unique<Effect>(graphics, effect_manager->get_effekseer_manager(), ".\\resources\\Effect\\enemy_vernier.efk");
+
+	//--------------------<弾の管理クラスを初期化>--------------------//
+	BulletManager& mBulletManager = BulletManager::Instance();
+	mBulletManager.fInitialize();
+	//--------------------<敵の管理クラスを初期化>--------------------//
+	mWaveManager.fInitialize(graphics, mBulletManager.fGetAddFunction());
+
+	player = std::make_unique<Player>(graphics);
+	// カメラ
+	cameraManager = std::make_unique<CameraManager>();
+
+	cameraManager->RegisterCamera(new GameCamera(player.get()));
+	cameraManager->RegisterCamera(new ClearCamera(player.get()));
+	//cameraManager->SetCamera(static_cast<int>(CameraTypes::Game));
+	//cameraManager->Initialize(graphics);
+	cameraManager->ChangeCamera(graphics, static_cast<int>(CameraTypes::Game));
+
+	// enemy_hp_gauge
+	enemy_hp_gauge = std::make_unique<EnemyHpGauge>(graphics);
+	// reticle
+	reticle = std::make_unique<Reticle>(graphics);
+	// wave
+	wave = std::make_unique<Counter>(graphics, L".\\resources\\Sprites\\ui\\wave.png");
+	// option
+	option = std::make_unique<Option>(graphics);
+	// tunnel
+	tunnel = std::make_unique<Tunnel>(graphics.get_device().Get());
+	//mini_map
+	minimap = std::make_unique<MiniMap>(graphics);
+}
+
+void TutorialScene::uninitialize()
+{
+	BulletManager& mBulletManager = BulletManager::Instance();
+
+	mWaveManager.fFinalize();
+	mBulletManager.fFinalize();
+}
+
+void TutorialScene::update(GraphicsPipeline& graphics, float elapsed_time)
+{
+	// option
+	if (option->get_validity())
+	{
+		option->update(graphics, elapsed_time);
+		return;
+	}
+	else
+	{
+		if (game_pad->get_button_down() & GamePad::BTN_START)
+		{
+			option->initialize();
+			option->set_validity(true);
+			return;
+		}
+	}
+	BulletManager& mBulletManager = BulletManager::Instance();
+
+	// クリア演出
+	mWaveManager.fUpdate(graphics, elapsed_time, mBulletManager.fGetAddFunction());
+	if (mWaveManager.during_clear_performance())
+	{
+		tunnel_alpha += elapsed_time * 0.5f;
+		tunnel_alpha = (std::min)(tunnel_alpha, 1.0f);
+
+		if (!during_clear)
+		{
+			cameraManager->ChangeCamera(graphics, static_cast<int>(CameraTypes::Tunnel));
+			player->TransitionStageMove();
+			during_clear = true;
+		}
+
+		//return;
+	}
+	else
+	{
+		if (during_clear)
+		{
+			tunnel_alpha -= elapsed_time;
+			tunnel_alpha = (std::max)(tunnel_alpha, 0.0f);
+			if (Math::equal_check(tunnel_alpha, 0.0f, 0.01f))
+			{
+				cameraManager->ChangeCamera(graphics, static_cast<int>(CameraTypes::Game));
+				player->TransitionIdle();
+				tunnel_alpha = 0.0f;
+				during_clear = false;
+			}
+		}
+	}
+
+	//--------------------<敵の管理クラスの更新処理>--------------------//
+
+	const auto enemyManager = mWaveManager.fGetEnemyManager();
+	enemyManager->fSetPlayerPosition(player->GetPosition());
+	mBulletManager.fUpdate(elapsed_time);
+
+	// ↓↓↓↓↓↓↓↓↓プレイヤーの更新はこのした↓↓↓↓↓
+	BaseEnemy* enemy = enemyManager->fGetNearestEnemyPosition();
+
+	Camera* c = cameraManager->GetCurrentCamera();
+
+	if (player->GetIsAlive() == false)	SceneManager::scene_switching(new SceneLoading(new SceneTitle()), DISSOLVE_TYPE::DOT, 2.0f);
+
+
+	// 敵とのあたり判定(当たったらコンボ加算)
+	if (player->GetIsPlayerAttack())
+	{
+		if (player->GetIsAwakening())
+		{
+			player->AddCombo(enemyManager->fCalcPlayerAttackVsEnemies(
+				player->GetSwordCapsuleParam(0).start,
+				player->GetSwordCapsuleParam(0).end,
+				player->GetSwordCapsuleParam(0).rasius,
+				player->GetPlayerPower()));
+
+			player->AddCombo(enemyManager->fCalcPlayerAttackVsEnemies(
+				player->GetSwordCapsuleParam(1).start,
+				player->GetSwordCapsuleParam(1).end,
+				player->GetSwordCapsuleParam(1).rasius,
+				player->GetPlayerPower()));
+		}
+		else
+		{
+			player->AddCombo(enemyManager->fCalcPlayerAttackVsEnemies(
+				player->GetSwordCapsuleParam(0).start,
+				player->GetSwordCapsuleParam(0).end,
+				player->GetSwordCapsuleParam(0).rasius,
+				player->GetPlayerPower()));
+		}
+	}
+
+	enemyManager->fCalcEnemiesAttackVsPlayer(player->GetBodyCapsuleParam().start,
+		player->GetBodyCapsuleParam().end,
+		player->GetBodyCapsuleParam().rasius, player->GetDamagedFunc());
+
+	//プレイヤーがジャスト回避した時の範囲スタンの当たり判定
+	enemyManager->fCalcPlayerStunVsEnemyBody(player->GetPosition(), player->GetStunRadius());
+
+	//弾とプレイヤーの当たり判定
+	mBulletManager.fCalcBulletsVsPlayer(player->GetBodyCapsuleParam().start,
+		player->GetBodyCapsuleParam().end,
+		player->GetBodyCapsuleParam().rasius, player->GetDamagedFunc());
+
+	// camera
+	//camera->Update(elapsed_time,player.get());
+	cameraManager->Update(elapsed_time);
+
+	player->SetCameraDirection(c->GetForward(), c->GetRight());
+	player->UpdateTutorial(elapsed_time, graphics, sky_dome.get(), enemyManager->fGetEnemies());
+	//player->UpdateTutorial(elapsed_time, graphics, sky_dome.get(), enemyManager->fGetEnemies());
+	player->lockon_post_effect(elapsed_time, [=](float scope, float alpha) { post_effect->lockon_post_effect(scope, alpha); },
+		[=]() { post_effect->clear_post_effect(); });
+	player->SetCameraPosition(c->get_eye());
+	player->SetTarget(enemy);
+	player->SetCameraTarget(c->get_target());
+	if (player->GetStartDashEffect()) post_effect->dash_post_effect(graphics.get_dc().Get(), player->GetPosition());
+	if (player->GetEndDashEffect())
+	{
+		post_effect->clear_post_effect();
+		player->SetEndDashEffect(false);
+	}
+
+
+	enemy_hp_gauge->update(graphics, elapsed_time);
+	enemy_hp_gauge->focus(player->GetPlayerTargetEnemy(), player->GetEnemyLockOn());
+
+	reticle->update(graphics, elapsed_time);
+	reticle->focus(player->GetPlayerTargetEnemy(), player->GetEnemyLockOn());
+	{
+		static DirectX::XMFLOAT2 pos{ 950.0f, 90.0f };
+		static DirectX::XMFLOAT2 offset{ 50.0f, 0 };
+		static DirectX::XMFLOAT2 scale{ 0.5f,0.5f };
+		static DirectX::XMFLOAT4 color{ 1,1,1,1 };
+		static int value{};
+		static bool is_display_imgui = false;
+		imgui_menu_bar("UI", "wave", is_display_imgui);
+#ifdef USE_IMGUI
+		if (is_display_imgui)
+		{
+			ImGui::Begin("wave");
+			ImGui::DragFloat2("pos", &pos.x);
+			ImGui::DragFloat2("offset", &offset.x);
+			ImGui::DragFloat2("scale", &scale.x, 0.01f);
+			ImGui::DragInt("value", &value);
+			ImGui::ColorEdit4("color", &color.x);
+			ImGui::End();
+		}
+#endif
+		wave->set_positoin(pos); wave->set_offset(offset); wave->set_scale(scale); wave->set_value(value); wave->set_color(color);
+		wave->update(graphics, elapsed_time);
+	}
+
+
+	//camera->update_with_quaternion(elapsed_time);
+	// shadow_map
+	shadow_map->debug_imgui();
+
+	//\effect_manager->update(elapsed_time);
+
+	// effect demo
+#ifdef USE_IMGUI
+	{
+		ImGui::Begin("effect");
+		if (ImGui::Button("play_effect"))
+		{
+			test_effect->play(effect_manager->get_effekseer_manager(), { 0,1,0 }, 3);
+		}
+		if (ImGui::Button("stop_effect"))
+		{
+			test_effect->stop(effect_manager->get_effekseer_manager());
+		}
+		{
+			static DirectX::XMFLOAT3 pos{};
+			ImGui::DragFloat3("position", &pos.x, 0.1f);
+			test_effect->set_position(effect_manager->get_effekseer_manager(), pos);
+			static DirectX::XMFLOAT3 scale{ 1,1,1 };
+			ImGui::DragFloat3("scale", &scale.x, 0.1f);
+			test_effect->set_scale(effect_manager->get_effekseer_manager(), scale);
+		}
+		ImGui::End();
+	}
+#endif // USE_IMGUI
+	// camera shake demo
+#ifdef USE_IMGUI
+	static bool is_shake = false;
+	ImGui::Begin("camera_shake");
+	if (ImGui::Checkbox("shake", &is_shake))
+	{
+		camera_shake->reset(graphics);
+	}
+	if (is_shake) camera_shake->shake(graphics, elapsed_time);
+	ImGui::End();
+#endif // USE_IMGUI
+	// hit_stop demo
+#ifdef USE_IMGUI
+	{
+		ImGui::Begin("hit_stop");
+		if (ImGui::Button("stop")) { hit_stop->damage_hit_stop(); }
+		ImGui::End();
+	}
+#endif // USE_IMGUI
+
+
+	// audio デモ
+	static float bgm_volume = 0.2f;
+	static float se_volume = 0.2f;
+	static bool is_open_button = { false };
+	static bool display_audio_imgui = { false };
+#ifdef USE_IMGUI
+	imgui_menu_bar("contents", "audio", display_audio_imgui);
+	if (display_audio_imgui)
+	{
+		if (ImGui::Begin("sound"))
+		{
+			if (ImGui::TreeNode("BGM"))
+			{
+				ImGui::DragFloat("bgm_volume", &bgm_volume, 0.1f, 0.0f, 3.0f);
+				//if (!is_open_button)
+				{
+					if (ImGui::Button("play bgm")) { audio_manager->play_bgm(BGM_INDEX::ENDING); is_open_button = true; }
+					if (ImGui::Button("play TITLE bgm")) { audio_manager->play_bgm(BGM_INDEX::TITLE); is_open_button = true; }
+				}
+				//else
+				{
+					if (ImGui::Button("stop bgm")) { audio_manager->stop_bgm(BGM_INDEX::ENDING); is_open_button = false; }
+				}
+				ImGui::TreePop();
+			}
+			if (ImGui::TreeNode("SE"))
+			{
+				ImGui::DragFloat("se_volume", &se_volume, 0.1f, 0.0f, 3.0f);
+				if (ImGui::Button("play se")) { audio_manager->play_se(SE_INDEX::DECISION); }
+				if (ImGui::Button("play item_get se")) { audio_manager->play_se(SE_INDEX::GET); }
+				ImGui::TreePop();
+			}
+			ImGui::End();
+		}
+	}
+#endif
+	audio_manager->set_volume_bgm(BGM_INDEX::ENDING, bgm_volume);
+	audio_manager->set_volume_bgm(BGM_INDEX::TITLE, bgm_volume);
+	audio_manager->set_volume_se(SE_INDEX::DECISION, se_volume);
+
+
+	//****************************************************************
+	//
+	// オブジェクトの削除処理はこの下でやるルール
+	//
+	//****************************************************************
+	enemyManager->fDeleteEnemies();
+
+}
+#define OFF_SCREEN_RENDERING
+#define SHADOW_MAP
+
+void TutorialScene::render(GraphicsPipeline& graphics, float elapsed_time)
+{
+#ifdef OFF_SCREEN_RENDERING
+	post_effect->begin(graphics.get_dc().Get());
+#endif // DEBUG
+
+	// シャドウマップのセット
+#ifdef SHADOW_MAP
+	shadow_map->set_shadowmap(graphics);
+#endif // SHADOW_MAP
+	// カメラのビュー行列計算
+	//camera->calc_view_projection(graphics);
+	cameraManager->CalcViewProjection(graphics);
+
+	/*-----!!!ここから上にオブジェクトの描画はしないで!!!!-----*/
+	{
+		graphics.set_pipeline_preset(BLEND_STATE::ALPHA, RASTERIZER_STATE::SOLID_COUNTERCLOCKWISE, DEPTH_STENCIL::DEON_DWON, SHADER_TYPES::DEFAULT);
+#ifdef USE_IMGUI
+		ImGui::Begin("sky");
+		ImGui::DragFloat("dimension", &dimension, 0.01f);
+		ImGui::End();
+#endif
+		sky_dome->Render(graphics, elapsed_time);
+
+
+	}
+
+
+	// font demo
+#if 0
+	{
+		graphics.set_pipeline_preset(BLEND_STATE::ALPHA, RASTERIZER_STATE::SOLID, DEPTH_STENCIL::DEOFF_DWOFF);
+		fonts->gothic->Begin(graphics.get_dc().Get());
+		fonts->gothic->Draw(L"テスト", { 640, 360 }, { 1,1 }, { 0,0,0,1 }, 0, TEXT_ALIGN::MIDDLE);
+		fonts->gothic->End(graphics.get_dc().Get());
+	}
+#endif // 0
+	BulletManager& mBulletManager = BulletManager::Instance();
+
+	//--------------------<敵の管理クラスの描画処理>--------------------//
+	mWaveManager.fGetEnemyManager()->fRender(graphics);
+	player->Render(graphics, elapsed_time);
+	mBulletManager.fRender(graphics);
+
+	//--------<ui>--------//
+	graphics.set_pipeline_preset(RASTERIZER_STATE::SOLID, DEPTH_STENCIL::DEOFF_DWOFF);
+	// enemy_hp_gauge
+	enemy_hp_gauge->render(graphics.get_dc().Get());
+	// reticle
+	reticle->render(graphics.get_dc().Get());
+	// wave
+	//wave->render(graphics.get_dc().Get());
+	Camera* c = cameraManager->GetCurrentCamera();
+	const DirectX::XMFLOAT2 p_pos = { player->GetPosition().x,player->GetPosition().z };
+	const DirectX::XMFLOAT2 c_forward = { c->GetForward().x,c->GetForward().z };
+	minimap->render(graphics, p_pos, c_forward, mWaveManager.fGetEnemyManager()->fGetEnemies());
+
+	effect_manager->render(Camera::get_keep_view(), Camera::get_keep_projection());
+	graphics.set_pipeline_preset(BLEND_STATE::ALPHA, RASTERIZER_STATE::WIREFRAME_CULL_BACK, DEPTH_STENCIL::DEON_DWON);
+	debug_figure->render_all_figures(graphics.get_dc().Get());
+
+
+	// クリア中のトンネル
+	if (during_clear)
+	{
+		graphics.set_pipeline_preset(RASTERIZER_STATE::SOLID, DEPTH_STENCIL::DEOFF_DWOFF);
+		tunnel->render(graphics.get_dc().Get(), elapsed_time, tunnel_alpha, [&]() {
+			player->Render(graphics, elapsed_time);
+			});
+	}
+
+	TutorialRender(graphics, elapsed_time);
+
+	/*-----!!!ここから下にオブジェクトの描画はしないで!!!!-----*/
+
+	// シャドウマップの破棄
+#ifdef SHADOW_MAP
+	shadow_map->clear_shadowmap(graphics);
+#endif // SHADOW_MAP
+
+#ifdef OFF_SCREEN_RENDERING
+	post_effect->end(graphics.get_dc().Get());
+	// 描画ステート設定
+	graphics.set_pipeline_preset(BLEND_STATE::ALPHA, RASTERIZER_STATE::SOLID, DEPTH_STENCIL::DEON_DWON);
+	// エフェクトをかける
+	post_effect->apply_an_effect(graphics.get_dc().Get(), elapsed_time);
+	post_effect->blit(graphics.get_dc().Get());
+	post_effect->scene_preview();
+#endif // DEBUG
+	// bloom
+	{
+		// 定数バッファにフェッチする
+		static bool display_bloom_imgui = false;
+#ifdef USE_IMGUI
+		imgui_menu_bar("contents", "bloom", display_bloom_imgui);
+		if (display_bloom_imgui)
+		{
+			ImGui::Begin("bloom");
+			ImGui::DragFloat("extraction_threshold", &bloom_constants->data.bloom_extraction_threshold, 0.1f);
+			ImGui::DragFloat("convolution_intensity", &bloom_constants->data.blur_convolution_intensity, 0.1f);
+			ImGui::End();
+		}
+#endif // USE_IMGUI
+		bloom_constants->bind(graphics.get_dc().Get(), 8);
+
+		graphics.set_pipeline_preset(BLEND_STATE::NO_PROCESS, RASTERIZER_STATE::CULL_NONE, DEPTH_STENCIL::DEOFF_DWOFF);
+		bloom_effect->make(graphics.get_dc().Get(), post_effect->get_color_map().Get());
+		graphics.set_pipeline_preset(BLEND_STATE::ADD, RASTERIZER_STATE::CULL_NONE, DEPTH_STENCIL::DEOFF_DWOFF);
+		bloom_effect->blit(graphics.get_dc().Get());
+	}
+
+	graphics.set_pipeline_preset(BLEND_STATE::ALPHA, RASTERIZER_STATE::SOLID, DEPTH_STENCIL::DEOFF_DWOFF);
+	mWaveManager.render(graphics.get_dc().Get(), elapsed_time);
+	if (option->get_validity()) { option->render(graphics, elapsed_time); }
+
+}
+
+void TutorialScene::register_shadowmap(GraphicsPipeline& graphics, float elapsed_time)
+{
+#ifdef SHADOW_MAP
+	Camera* c = cameraManager->GetCurrentCamera();
+	//--シャドウマップの生成--//
+	shadow_map->activate_shadowmap(graphics, c->get_light_direction());
+
+
+
+
+
+
+
+	//--元のビューポートに戻す--//
+	shadow_map->deactivate_shadowmap(graphics);
+#endif // SHADOW_MAP
+
+}
+
+void TutorialScene::TutorialUpdate(GraphicsPipeline& graphics, float elapsed_time)
+{
+	switch (tutorial_state)
+	{
+	case TutorialScene::TutorialState::MoveTutorial:
+		player->ChangeTutorialState(static_cast<int>(tutorial_state));
+		break;
+	case TutorialScene::TutorialState::AvoidanceTutorial:
+		player->ChangeTutorialState(static_cast<int>(tutorial_state));
+		break;
+	case TutorialScene::TutorialState::LockOnTutorial:
+		player->ChangeTutorialState(static_cast<int>(tutorial_state));
+		break;
+	case TutorialScene::TutorialState::AttackTutorial:
+		player->ChangeTutorialState(static_cast<int>(tutorial_state));
+		break;
+	case TutorialScene::TutorialState::BehindAvoidanceTutorial:
+		player->ChangeTutorialState(static_cast<int>(tutorial_state));
+		break;
+	case TutorialScene::TutorialState::ChainAttackTutorial:
+		player->ChangeTutorialState(static_cast<int>(tutorial_state));
+		break;
+	case TutorialScene::TutorialState::AwaikingTutorial:
+		player->ChangeTutorialState(static_cast<int>(tutorial_state));
+		break;
+	default:
+		break;
+	}
+}
+
+void TutorialScene::TutorialRender(GraphicsPipeline& graphics, float elapsed_time)
+{
+}
--- sources/SceneTutorial.h
+++ sources/SceneTutorial.h
@@ -1,1 +1,124 @@
 #pragma once
+#include "Scene.h"
+#include "camera.h"
+#include "CameraManager.h"
+#include "shadow_map.h"
+#include "post_effect.h"
+#include "bloom.h"
+#include "practical_entities.h"
+#include "sprite_batch.h"
+#include "skinned_mesh.h"
+#include "effect.h"
+#include "point_light.h"
+#include "husk_particles.h"
+#include "constants.h"
+#include "imgui_include.h"
+#include"WaveManager.h"
+#include "mini_map.h"
+#include"EnemyManager.h"
+#include"Player.h"
+#include"SkyDome.h"
+#include "enemy_hp_gauge.h"
+#include "reticle.h"
+#include "counter.h"
+#include "scene_option.h"
+#include "BulletManager.h"
+#include "tunnel.h"
+
+class TutorialScene : public Scene, public PracticalEntities
+{
+public:
+    TutorialScene() {}
+    ~TutorialScene() override {}
+    //初期化
+    void initialize(GraphicsPipeline& graphics) override;
+    //終了化
+    void uninitialize() override;
+    //更新処理
+    void update(GraphicsPipeline& graphics, float elapsed_time) override;
+    //描画処理
+    void render(GraphicsPipeline& graphics, float elapsed_time) override;
+    //シャドウマップ登録
+    void register_shadowmap(GraphicsPipeline& graphics, float elapsed_time) override;
+private:
+    void TutorialUpdate(GraphicsPipeline& graphics, float elapsed_time);
+    //チュートリアルの文字とかを描画する
+    void TutorialRender(GraphicsPipeline& graphics, float elapsed_time);
+private:
+    //--------< 変数 >--------//
+    // camera
+    //std::unique_ptr<Camera> camera;
+    std::unique_ptr<CameraManager> cameraManager;
+public:
+    enum class CameraTypes
+    {
+        Game,
+        Tunnel,
+    };
+
+    //1が最初で大きくなっていくようにする
+    enum class TutorialState
+    {
+        //移動
+        MoveTutorial = 1,
+        //回避(通常)
+        AvoidanceTutorial,
+        //ロックオン
+        LockOnTutorial,
+        //攻撃
+        AttackTutorial,
+        //回り込み回避
+        BehindAvoidanceTutorial,
+        //チェイン攻撃
+        ChainAttackTutorial,
+        //覚醒
+        AwaikingTutorial
+    };
+
+    TutorialState tutorial_state{ TutorialState::MoveTutorial };
+
+private:
+    // shadowmap
+    std::unique_ptr<ShadowMap> shadow_map;
+    // post effect
+    std::unique_ptr<PostEffect> post_effect;
+    // bloom
+    std::unique_ptr<Bloom> bloom_effect;
+    struct BloomConstants
+    {
+        float bloom_extraction_threshold = 0.800f;
+        float blur_convolution_intensity = 0.500f;
+
+        float bloom_options[2];
+    };
+    std::unique_ptr<Constants<BloomConstants>> bloom_constants;
+    //// スキンメッシュの実体
+    //std::shared_ptr<SkinnedMesh> sky_dome;
+    std::unique_ptr<Effect> test_effect;
+    //****************************************************************
+    //
+    // スピック開始後追加した変数
+    //
+    //****************************************************************
+    //EnemyManager mEnemyManager{};
+    WaveManager mWaveManager{};
+    std::unique_ptr<Player> player{ nullptr };
+    std::unique_ptr<SkyDome> sky_dome{ nullptr };
+    //skydome
+    float dimension{ 0.1f };
+    // enemy_hp_gauge
+    std::unique_ptr<EnemyHpGauge> enemy_hp_gauge{ nullptr };
+    // reticle
+    std::unique_ptr<Reticle> reticle{ nullptr };
+    // counter
+    std::unique_ptr<Counter> wave{ nullptr };
+    // option
+    std::unique_ptr<Option> option{ nullptr };
+    std::unique_ptr<MiniMap> minimap{ nullptr };
+
+    // クリア演出
+    bool during_clear = false;
+    float tunnel_alpha = 0.0f;
+    std::unique_ptr<Tunnel> tunnel{ nullptr };
+
+};
