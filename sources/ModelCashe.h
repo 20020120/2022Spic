77bad71846834e90ceb17a34e8fb773acb192bf4
umiyuriktn <2200114@ecccomp.com>
2022-05-11 23:20:05 +0900

    モデルキャッシュ修正、ロード画面でも音量調整
    モデルキャッシュ修正、ロード画面でも音量調整

--- sources/ModelCashe.h
+++ sources/ModelCashe.h
@@ -16,18 +16,8 @@ private:
 inline void ModelCashes::Load_IntoTitle(ID3D11Device* pDevice_)
 {
     // ボスのモデルをロード
-    auto model1 = resource_manager->load_model_resource(pDevice_, "./resources/Models/Enemy/boss_animation_second.fbx");
+    auto model1 = resource_manager->load_model_resource(pDevice_, "./resources/Models/Enemy/boss_animation_third.fbx");
     mpModelCashVec.emplace_back(std::move(model1));
-    //auto model2 = resource_manager->load_model_resource(pDevice_, "./resources/Models/Enemy/ArcherEnemy.fbx");
-    //mpModelCashVec.emplace_back(std::move(model2));
-    //auto model3 = resource_manager->load_model_resource(pDevice_, "./resources/Models/Enemy/SpearEnemy.fbx");
-    //mpModelCashVec.emplace_back(std::move(model3));
-    //auto model4 = resource_manager->load_model_resource(pDevice_, "./resources/Models/Enemy/SpearEnemyAce.fbx");
-    //mpModelCashVec.emplace_back(std::move(model4));
-    //auto model5 = resource_manager->load_model_resource(pDevice_, "./resources/Models/Enemy/SwordEnemy.fbx");
-    //mpModelCashVec.emplace_back(std::move(model5));
-    //auto model6 = resource_manager->load_model_resource(pDevice_, "./resources/Models/Enemy/SwordEnemyAce.fbx");
-    //mpModelCashVec.emplace_back(std::move(model6));
 }
 
 inline void ModelCashes::Load_PreTitle(ID3D11Device* pDevice_)
@@ -36,5 +26,4 @@ inline void ModelCashes::Load_PreTitle(ID3D11Device* pDevice_)
     mpModelCashVec.emplace_back(std::move(model1));
     auto model2 = resource_manager->load_model_resource(pDevice_, ".\\resources\\Models\\stage\\title_stage.fbx");
     mpModelCashVec.emplace_back(std::move(model2));
-
 }
