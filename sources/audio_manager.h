f7ec543d05b8f734008f9b7761d150144d4ed3a2
act333 <rikey.614@gmail.com>
2022-05-17 15:33:58 +0900

    se追加

--- sources/audio_manager.h
+++ sources/audio_manager.h
@@ -54,6 +54,8 @@ enum class SE_INDEX
     FIRE_BOLL_RUN,          //火の玉が走る
     ROCK_ON,                //ロックオン時の音
     PLAYER_RUSH,            //プレイヤー突進
+    SPEAR_RUSH,             //槍の敵突進
+    BOSS_RUSH,              //ボス突進
 
     SE_COUNT
 };
