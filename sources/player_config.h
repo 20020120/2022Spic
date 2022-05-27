ea7e9a4abaee3f7651bb3099047f1095e15d8e3a
umiyuriktn <2200114@ecccomp.com>
2022-05-27 10:51:30 +0900

    mp maxの時演出
    mp maxの時演出

--- sources/player_config.h
+++ sources/player_config.h
@@ -25,13 +25,24 @@ private:
     std::unique_ptr<SpriteBatch> base_sprite{ nullptr };
     std::unique_ptr<SpriteBatch> base2_sprite{ nullptr };
 
+    std::unique_ptr<SpriteBatch> mp_body_max_sprite{ nullptr };
+    std::unique_ptr<SpriteBatch> mp_body_max1_sprite{ nullptr };
+    std::unique_ptr<SpriteBatch> mp_body_max2_sprite{ nullptr };
+    std::unique_ptr<SpriteBatch> mp_body_max3_sprite{ nullptr };
+
     Element base;
     Element hp_gauge;
     Element hp_body;
     Element mp_gauge;
     Element mp_body;
+    Element mp_electric;
     float hp_percent = 1.0f;
     float mp_percent = 1.0f;
 
     float glow_vertical = {};
+
+    float electric_timer = 0;
+    float electric_wait_timer = 0;
+    bool mp_max = false;
+    bool end_of_anim = false;
 };
